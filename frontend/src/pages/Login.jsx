a808b799fdb7e4cff9c3ebdbc4e2705a82b4b524
Giorgos <giorgos2532018@yahoo.com>
2025-06-18 20:35:50 +0300

    i add frontend

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,7 @@
+import Form from "../components/Form"
+
+function Login() {
+    return <Form route="/website/token/" method="login" />
+}
+
+export default Login
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,7 @@
+import Form from "../components/Form"
+
+function Register() {
+    return <Form route="/website/user/register/" method="register" />
+}
+
+export default Register
